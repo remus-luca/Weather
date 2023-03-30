acfe3dae85ea158b266a3585ea38d7c95f77fa85
remus-luca <lucaremusmarian@yahoo.com>
2023-03-30 20:48:21 +0300

    fetch data from api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "", // enter your rapid api key here
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
+export const WEATHER_API_KEY = "9d3a92fa1c51d4234a02e3b6a5f24107";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "";
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,40 +1,42 @@
 import "./current-weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = (data) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
         <img
-          src={"/src/Icons/01d.png"}
+          src={`/src/Icons/${data.weather[0].icon}.png`}
           alt="weather"
           className="weather-icon"
         />
       </div>
       <div className="bottom">
-        <div className="temperature">18°C</div>
+        <div className="temperature">{Math.round(data.main.temp)}°C</div>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label ">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels Like</span>
-            <span className="parameter-value">22°C</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind Like</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed}m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15hPa</span>
+            <span className="parameter-value">{data.main.pressure}hPa</span>
           </div>
         </div>
       </div>
